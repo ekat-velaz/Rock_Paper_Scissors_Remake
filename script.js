250e9c0f943fcb0a3cd8d09a2cd2d177ab0a1601
ekaterina <katyabalash@mail.ru>
2022-10-04 15:45:47 +0300

    Adjust DOM textContent

--- script.js
+++ script.js
@@ -19,15 +19,23 @@ btnScissors.addEventListener("click", () => {
   playRound();
 });
 
-
+let playerSelTexr = document.querySelector(".playerChoice");
+let compSelText = document.querySelector(".computerChoice");
+let roundResText = document.querySelector(".roundResult");
+let playerScoreText = document.querySelector(".playerScore");
+let compScoreText = document.querySelector(".computerScore");
+let finalResText = document.querySelector(".finalResult");
 
 function getComputerChoice(){
     let number=Math.floor(Math.random()*3);
     if (number===0) {
+        compSelText.textContent = "Computer's choice is: Rock.";
         return "Computer's choice is: Rock.";
     } else if (number===1) {
+      compSelText.textContent = "Computer's choice is: Paper.";
        return "Computer's choice is: Paper.";
     } else if (number===2) {
+      compSelText.textContent =  "Computer's choice is: Scissors.";
        return "Computer's choice is: Scissors.";
     };
 };
@@ -49,10 +57,13 @@ function getPlayerChoice() {
      return "You didnt choose anything..";
     } else {
     if ( answer === "Rock") {
+      playerSelTexr.textContent = "You've chosen Rock!";
       return ("You've chosen Rock!");
     } else if (answer === "Paper") {
+      playerSelTexr.textContent = "You've chosen Paper!";
       return "You've chosen Paper!";
     } else if (answer === "Scissors") {
+      playerSelTexr.textContent = "You've chosen Scissors!";
       return "You've chosen Scissors!";
     } else {
       return "Try again!";
@@ -63,14 +74,14 @@ function getPlayerChoice() {
 let computerChoice; 
 let playerChoice;
 let roundResult;
+let computerScore = 0;
+let playerScore = 0;
+let finalResult;
 
-function logResults() {
-  console.log(playerChoice);
-  console.log(computerChoice);
-  console.log(roundResult);
-};
 
 function playRound (computerChoice , playerChoice) {
+  compScoreText.textContent = `Computer score is: ${computerScore}.`;
+  playerScoreText.textContent = `Your score is: ${playerScore}`;
     computerChoice = getComputerChoice();
     playerChoice = getPlayerChoice();
     console.log(playerChoice);
@@ -78,49 +89,54 @@ function playRound (computerChoice , playerChoice) {
     if ((computerChoice == "Computer's choice is: Rock." && playerChoice == "You've chosen Rock!") || 
     (computerChoice == "Computer's choice is: Paper." && playerChoice == "You've chosen Paper!") ||
     (computerChoice == "Computer's choice is: Scissors." && playerChoice == "You've chosen Scissors!")) {
+      roundResText.textContent = "It's a tie!";
       return roundResult = "It's a tie!";
     } else if (computerChoice == "Computer's choice is: Rock." && playerChoice == "You've chosen Paper!") {
-      return roundResult= "You won! Paper beats Rock.";
+      roundResText.textContent ="You won! Paper beats Rock.";
+       roundResult= "You won! Paper beats Rock.";
     } else if (computerChoice == "Computer's choice is: Paper." && playerChoice == "You've chosen Rock!") {
-        return roundResult = "You lost! Paper beats Rock.";
+      roundResText.textContent = "You lost! Paper beats Rock.";
+       roundResult = "You lost! Paper beats Rock.";
     } else if (computerChoice == "Computer's choice is: Paper." && playerChoice == "You've chosen Scissors!") {
-        return roundResult = "You won! Scissors beats Paper.";
+      roundResText.textContent = "You won! Scissors beats Paper.";
+       roundResult = "You won! Scissors beats Paper.";
     } else if (computerChoice == "Computer's choice is: Scissors." && playerChoice == "You've chosen Paper!") {
-        return roundResult = "You lost! Scissors beats Paper!";
+      roundResText.textContent = "You lost! Scissors beats Paper!";
+       roundResult = "You lost! Scissors beats Paper!";
     } else if (computerChoice == "Computer's choice is: Rock." && playerChoice == "You've chosen Scissors!") {
-        return roundResult = "You lost! Rock beats Scissors.";
+      roundResText.textContent = "You lost! Rock beats Scissors.";
+      return roundResult = "You lost! Rock beats Scissors.";
     } else if (computerChoice == "Computer's choice is: Scissors." && playerChoice == "You've chosen Rock!") {
-        return roundResult = "You won! Rock beats Scissors.";
+      roundResText.textContent = "You won! Rock beats Scissors.";
+       roundResult = "You won! Rock beats Scissors.";
     } else {
-        return roundResult = "Try again!";
+         roundResult = "Try again!";
     };
     
+    countScore();
+    getFinalResult();
 };
 
-let computerScore = 0;
-let playerScore = 0;
-let finalResult;
-
-function game() {
- for (i = 0; i <= 5; i++) {
-     console.log(playRound(computerChoice , playerChoice));
-     if (roundResult == "You won! Paper beats Rock." || roundResult == "You won! Scissors beats Paper." || roundResult == "You won! Rock beats Scissors.") {
-      playerScore = ++playerScore;
-     } else if (roundResult == "You lost! Paper beats Rock." || roundResult == "You lost! Scissors beats Paper!" || roundResult == "You lost! Rock beats Scissors.") {
-      computerScore = ++computerScore;
-     };
-     console.log(`Computer score is: ${computerScore}.`);
-     console.log(`Your score is: ${playerScore}`);
-      };
- if (computerScore > playerScore) {
-  return finalResult = "Sorry.. Computer beaten you in this game."
- } else if (computerScore < playerScore) {
-  return finalResult = "Yay! You won completely!";
- } else if (computerScore == playerScore){
-  return finalResult = "Haha lol its a tie!";
- };
- console.log(finalResult);
+function countScore() {
+   if (roundResult == "You won! Paper beats Rock." || roundResult == "You won! Scissors beats Paper." || roundResult == "You won! Rock beats Scissors.") {
+    playerScore = ++playerScore;
+   } else if (roundResult == "You lost! Paper beats Rock." || roundResult == "You lost! Scissors beats Paper!" || roundResult == "You lost! Rock beats Scissors.") {
+    computerScore = ++computerScore;
+   }; 
+   compScoreText.textContent = `Computer score is: ${computerScore}.`;
+   playerScoreText.textContent = `Your score is: ${playerScore}`;
 };
 
-
+function getFinalResult() {
+  if (computerScore === 5 || playerScore === 5) {
+  if (computerScore > playerScore) {
+    finalResult = "Sorry.. Computer beaten you in this game."
+   } else if (computerScore < playerScore) {
+    finalResult = "Yay! You won completely!";
+   } else if (computerScore == playerScore){
+     finalResult = "Haha lol its a tie!";
+   };
+   finalResText.textContent = `${finalResult}`;
+  };
+};
 
